b9c6653a3c3f9021e41ce3406a3386daa1dbd8ac
isokissa3 <koti94@windowslive.com>
2019-01-08 19:16:57 +0200

    Quick hack for nmshandler

--- Bukkit/src/main/java/fi/joniaromaa/parinacorelibrary/bukkit/nms/NmsManager.java
+++ Bukkit/src/main/java/fi/joniaromaa/parinacorelibrary/bukkit/nms/NmsManager.java
@@ -4,9 +4,24 @@ import fi.joniaromaa.parinacorelibrary.bukkit.nms.handlers.NsmHandler;
 
 public class NmsManager
 {
-	//TODO: Implement this actually
+	private static NsmHandler nmsHandler;
+	
+	static
+	{
+		try
+		{
+			Class<?> clazz = Class.forName("fi.joniaromaa.parinacorelibrary.bukkit.nms.v1_8_R3.NsmHandler");
+			
+			NmsManager.nmsHandler = (NsmHandler)clazz.newInstance();
+		}
+		catch (ClassNotFoundException | InstantiationException | IllegalAccessException e)
+		{
+			
+		}
+	}
+	
 	public static NsmHandler getNmsHandler()
 	{
-		return null;
+		return NmsManager.nmsHandler;
 	}
 }
